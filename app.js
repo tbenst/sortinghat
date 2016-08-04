84cdc3a94067008ce2e6447024127e58f5ae699b
tbenst <tbenst@gmail.com>
2016-08-03 23:00:02 -0700

    making progress..

---
+++ app.js
@@ -0,0 +1,24 @@
+var koa = require('koa');
+var app = koa();
+
+
+
+// logger
+
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(require('koa-static')('static'));
+
+// response
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(4000);
